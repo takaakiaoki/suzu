950502922b2cec4e151baec90f52024fa9db42f2
Takaaki AOKI <aoki@sakura.nucleng.kyoto-u.ac.jp>
2013-04-07 06:29:29 +0900

    layout of layer_elem.py tuned

--- dummy_tin/layer_elem.py
+++ dummy_tin/layer_elem.py
@@ -34,63 +34,66 @@ class LayerElem(tk.Frame, ga.GUIAbstract):
         ga.GUIAbstract.__init__(self, defaultparam=LayerElem.defaultparam)
 
         prow = 0
+        epadx = (15, 0) # x inner padding for Entries
 
         # name
-        self.namelabel = tk.Label(self, text='Layer Name')
-        self.name = ga.TunedEntry(self)
+        self.namefrm = tk.Frame(self)
+        self.namelabel = tk.Label(self.namefrm, text='Layer Name')
+        self.name = ga.TunedEntry(self.namefrm)
         self.add_widget('name', self.name)
-        self.namelabel.grid(row=prow, column=0, columnspan=3, sticky=tk.W)
-        prow += 1
-        # dummy frame
-        tk.Frame(self).grid(row=prow, column=0, ipadx=5)
-        self.name.grid(row=prow, column=1, columnspan=2, sticky=tk.W)
+        self.namelabel.grid(row=0, column=0, sticky=tk.SW)
+        self.name.grid(row=1, column=0, padx=epadx, sticky=tk.NW)
+        self.namefrm.grid(row=prow, column=0, sticky=tk.W)
         prow += 1
 
-        # fill very small space
-        tk.Frame(self).grid(row=prow, column=0, ipady=3)
-        prow += 1
-
+        # width & wunit
+        self.widthfrm = tk.Frame(self)
         # width
-        self.widthlabel = tk.Label(self, text='Width')
-        self.width = tktool.validateentry.DoublePositive(self, width=10, justify=tk.RIGHT)
+        self.widthlabel = tk.Label(self.widthfrm, text='Width')
+        self.width = tktool.validateentry.DoublePositive(self.widthfrm, width=10, justify=tk.RIGHT)
         self.add_widget('width', self.width)
-        self.widthlabel.grid(row=prow, column=0, columnspan=3, sticky=tk.W)
-        prow += 1
-        self.width.grid(row=prow, column=1, sticky=tk.W)
-
         # wunit
         self.wunitopts = [('Ang', 'Ang'), ('um', 'um'), ('mm', 'mm'),
                 ('cm', 'cm'), ('m', 'm'), ('km', 'km')]
-
-        self.wunit = coption.CodedOptionMenu(self, self.wunitopts)
+        self.wunit = coption.CodedOptionMenu(self.widthfrm, self.wunitopts)
         self.wunit.config(width=5)
         self.add_widget('wunit', self.wunit)
-        self.wunit.grid(row=prow, column=2, sticky=tk.W)
+
+        self.widthlabel.grid(row=0, column=0, columnspan=2, sticky=tk.SW)
+        self.width.grid(row=1, column=0, padx=epadx, sticky=tk.W)
+        self.wunit.grid(row=1, column=1, sticky=tk.NW)
+        self.widthfrm.grid(row=prow, column=0, sticky=tk.W)
         prow += 1
 
         # dens
-        self.denslabel = tk.Label(self, text='Density (g/cm3)')
-        self.dens = tktool.validateentry.DoublePositive(self, width=10, justify=tk.RIGHT)
+        self.densfrm = tk.Frame(self)
+        self.denslabel = tk.Label(self.densfrm, text='Density (g/cm3)')
+        self.dens = tktool.validateentry.DoublePositive(self.densfrm, width=10, justify=tk.RIGHT)
         self.add_widget('dens', self.dens)
-        self.denslabel.grid(row=prow, column=0, columnspan=3, sticky=tk.W)
-        prow += 1
-        self.dens.grid(row=prow, column=1, sticky=tk.W)
+
+        self.denslabel.grid(row=0, column=0, sticky=tk.SW)
+        self.dens.grid(row=1, column=0, padx=epadx, sticky=tk.NW)
+
+        self.densfrm.grid(row=prow, column=0, sticky=tk.W)
         prow += 1
 
         # corr
-        self.corrlabel = tk.Label(self, text='Compound Corr.')
-        self.corr = tktool.validateentry.DoublePositive(self, width=10, justify=tk.RIGHT)
+        self.corrfrm = tk.Frame(self)
+        self.corrlabel = tk.Label(self.corrfrm, text='Compound Corr.')
+        self.corr = tktool.validateentry.DoublePositive(self.corrfrm, width=10, justify=tk.RIGHT)
         self.add_widget('corr', self.corr)
-        self.corrlabel.grid(row=prow, column=0, columnspan=3, sticky=tk.W)
+
+        self.corrlabel.grid(row=0, column=0, columnspan=3, sticky=tk.SW)
+        self.corr.grid(row=1, column=0, padx=epadx, sticky=tk.NW)
+
+        self.corrfrm.grid(row=prow, column=0, sticky=tk.W)
         prow += 1
-        self.corr.grid(row=prow, column=1, sticky=tk.W)
-        prow += 1
-        self.clear()
 
         # gas
         self.gas = ga.TunedCheckbuttonTF(self, text='Gas Material')
         self.add_widget('gas', self.gas)
-        self.gas.grid(row=prow, column=0, columnspan=3, sticky=tk.W)
+        self.gas.grid(row=prow, column=0, sticky=tk.W)
+        prow += 1
 
         # atomtbl
         self.atomtblfrm = tk.LabelFrame(self, text='Atoms')
@@ -98,6 +101,8 @@ class LayerElem(tk.Frame, ga.GUIAbstract):
         self.atomtbl.pack()
         self.add_widget('atomtbl', self.atomtbl)
         self.atomtblfrm.grid(row=0, column=4, rowspan=prow, padx=10)
+
+        self.clear()
 
 if __name__ == '__main__':
     import tktool.gui_testframe as gt
