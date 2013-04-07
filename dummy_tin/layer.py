ea72b3c0522c3c764e30c239d2ade5585f9fd54a
Takaaki AOKI <aoki@sakura.nucleng.kyoto-u.ac.jp>
2013-04-07 10:07:37 +0900

    layout of layer is modified. For this purpose tktool.OneofFactory have layout option

--- dummy_tin/layer.py
+++ dummy_tin/layer.py
@@ -9,8 +9,23 @@ import context
 defaultparam = context.layer_default
 exampleparam = context.layer_example
 
+layout = {
+        # ctrl frame, relative to root frame
+        'ctrl':{'row':0, 'column':0, 'sticky':tk.NE, 'padx':(0,10)},
+        # element view, relative to root frame
+        'view':{'row':0, 'column':1, 'sticky':tk.W},
+        # control parts which are placed in ctrlfrm in usual
+        # if a widget should be placed in relative to root frame
+        # set 'root' for key 'in', otherwise it is placed in ctrl frame
+        'index':{'in':'ctrl', 'row':0, 'column':0, 'sticky':tk.W},
+        'totallabel':{'row':0, 'column':1, 'sticky':tk.W}, # 'in' key can be ommitted.
+        'append':{'row':1, 'column':0, 'columnspan':2, 'sticky':tk.EW},
+        'delete':{'row':2, 'column':0, 'columnspan':2, 'sticky':tk.EW}
+        }
+
 Layer = tktool.oneof.OneofFactory(layer_elem.LayerElem,
-        layer_elem.LayerElem.defaultparam)
+        layer_elem.LayerElem.defaultparam,
+        layout)
 
 if __name__ == '__main__':
     import tktool.gui_testframe as gt
