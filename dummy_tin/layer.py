08d48b3d9edc62e74b469869ee12c43aaf3b96bb
Takaaki AOKI <aoki@sakura.nucleng.kyoto-u.ac.jp>
2013-04-10 17:06:55 +0900

    changed dummy_tin/layer.py to validate sum of stoichmetry for each layer.

--- dummy_tin/layer.py
+++ dummy_tin/layer.py
@@ -23,15 +23,69 @@ layout = {
         'delete':{'row':2, 'column':0, 'columnspan':2, 'sticky':tk.EW}
         }
 
-Layer = tktool.oneof.OneofFactory(layer_elem.LayerElem,
+LayerBase = tktool.oneof.OneofFactory(layer_elem.LayerElem,
         layer_elem.LayerElem.defaultparam,
         layout)
 
+class Layer(LayerBase):
+    def __init__(self, master=None, gridlayout=None, *args, **kw):
+        LayerBase.__init__(self, master, gridlayout, *args, **kw)
+
+    def validate(self):
+        if self.is_disabled():
+            return None
+
+        # 1st level validation for each widget
+        err = LayerBase.validate(self)
+        if err:
+            return err
+
+        err = []
+        errorlayers = []
+
+        # 2nd level validation 
+        # sum of stoich not should be 0
+        # store current view, it must pass because contents of
+        # view is already validated
+        self.store_currentview()
+        for i, edata in enumerate(self.elemdata):
+            ss = 0.0
+            if 'atomtbl' in edata:
+                for e in edata['atomtbl']:
+                    ss += e['stoich']
+            if ss < 1e-6:
+                errorlayers.append(i)
+                err.append(('layer {0:d}'.format(i+1),
+                    'sum of stoich. is zero (or too small)'))
+
+        if err:
+            # set first error layer on view and set alert face
+            self.index_action_force(errorlayers[0])
+            # set alert on stoich column
+            self.view.atomtbl.alert_stoich_column()
+        else:
+            # clear alert on stoich column 
+            self.view.atomtbl.noalert_stoich_column()
+
+        return err if err else None
+
+
+
 if __name__ == '__main__':
-    import tktool.gui_testframe as gt
+    import tktool
+    import copy
 
     app = tk.Tk()
 
-    gt.gui_testframe(app, Layer, exampleparam)
+    ex2 = copy.deepcopy(exampleparam)
+
+    # example 2 stoichimetry of layer 2 is too small
+    ex2[1]['atomtbl'][0]['stoich'] = 0
+    ex2[1]['atomtbl'][1]['stoich'] = 0
+
+    examples = [('standard', exampleparam),
+            ('stoich. error in 2nd layer', ex2)]
+
+    tktool.gui_testframe_multiexam(app, Layer, examples)
 
     app.mainloop()
