381b4b547476084a3a3da5c57fc879b68896d574
Takaaki AOKI <aoki@sakura.nucleng.kyoto-u.ac.jp>
2013-04-12 18:17:00 +0900

    preparation for v0.0.4

--- dummy_tin/version.py
+++ dummy_tin/version.py
@@ -1,7 +1,7 @@
 _version_major = 0
 _version_minor = 0
 _version_micro = 4
-_version_extra = 'pre'
+_version_extra = ''
 
 __version__ = "{0:d}.{1:d}.{2:d}{3:s}".format(
         _version_major, _version_minor, _version_micro, _version_extra)
