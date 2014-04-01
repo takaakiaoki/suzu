44e6c3733372f81d2e9484fdd3754e455a8194fd
Takaaki AOKI <aoki@sakura.nucleng.kyoto-u.ac.jp>
2014-04-01 18:35:42 +0900

    cx_freeze is introduced insted of py2exe
    --HG--
    branch : py3.3.5

--- dummy_tin/version.py
+++ dummy_tin/version.py
@@ -1,7 +1,7 @@
 _version_major = 0
 _version_minor = 0
 _version_micro = 5
-_version_extra = 'pre'
+_version_extra = 0
 
-__version__ = "{0:d}.{1:d}.{2:d}{3:s}".format(
-        _version_major, _version_minor, _version_micro, _version_extra)
+__version__ = "{0:d}.{1:d}.{2:d}".format(
+        _version_major, _version_minor, _version_micro)
--- tools/suzu_version.py
+++ tools/suzu_version.py
@@ -7,7 +7,7 @@ ver_file = os.path.join(os.path.dirname(__file__), '..', 'dummy_tin', 'version.p
 vars = {}
 exec(open(ver_file).read(), vars)
 
-print vars['__version__']
+print(vars['__version__'])
 
 # to environment variable in cmd.exe
 # for /f usebackq %%1 in (`python suzu_version.py`) do (@set VAR=%%1)
