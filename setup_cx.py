d2e069c5ce243c5a343578cecbcf5b98353320ef
Takaaki AOKI <aoki.takaaki.6v@kyoto-u.ac.jp>
2018-04-18 19:52:26 +0900

    modified setup_cx.py to run on Py3.5

--- setup_cx.py
+++ setup_cx.py
@@ -4,6 +4,8 @@ import glob
 
 from cx_Freeze import setup, Executable
 
+from tools.find_in_pathenv import find_in_pathenv
+
 ver_file = os.path.join(os.path.dirname(__file__), 'suzu', 'version.py')
 vars = {}
 exec(open(ver_file).read(), vars)
@@ -22,8 +24,10 @@ def create_include_tix():
     tk_version = _tkinter.TK_VERSION 
     tix_version = tk.call('package', 'version', 'Tix')
 
-    # get tcl library directory
-    tcl_dir = tk.call('info', 'library')
+    # get tcl and tk library directory
+    # see https://stackoverflow.com/questions/35533803/keyerror-tcl-library-when-i-use-cx-freeze
+    tcl_dir = tk.exprstring('$tcl_library')
+    tk_dir = tk.exprstring('$tk_library')
 
     del tkinter, _tkinter
 
@@ -31,17 +35,33 @@ def create_include_tix():
     print('tk_version = ', tk_version)
     print('tix_version = ', tix_version)
     print('tcl_dir = ', tcl_dir)
+    print('tk_dir = ', tk_dir)
+
+    includelist = []
+
+    # set os.environ, 
+    # see https://stackoverflow.com/questions/35533803/keyerror-tcl-library-when-i-use-cx-freeze
+    os.environ['TCL_LIBRARY'] = tcl_dir
+    os.environ['TK_LIBRARY'] = tk_dir
+
+    # tcl and tk dlls
+    tcldllpath = find_in_pathenv('tcl{}t.dll'.format(tcl_version.replace('.', '')))
+
+    if tcldllpath is None:
+        raise 'tcl{xx}t.dll is not found'
+
+    tkdllpath = find_in_pathenv('tk{}t.dll'.format(tk_version.replace('.', '')))
+    if tkdllpath is None:
+        raise 'tk{xx}t.dll is not found'
 
     # try to find tix directory
     tix_basename = 'tix{}'.format(tix_version)
     tix_dir = os.path.join(os.path.dirname(tcl_dir), tix_basename)
     print('tix_dir = ', tix_dir)
-    if os.path.isdir(tix_dir):
-        return [(tix_dir, tix_basename)]
-    else:
-        print('tix_dir does not exist.')
+    if not os.path.isdir(tix_dir):
+        raise 'tix_dir does not exist.'
 
-    return []
+    return [str(tcldllpath), str(tkdllpath), (tix_dir, tix_basename)]
 
 def create_include_doc():
     htmls = glob.glob('suzu/doc/*.html')
@@ -52,7 +72,7 @@ def create_include_doc():
         fs.append((f, os.path.join('doc', basename)))
     return fs
 
-include_files = [('README','README'), ('README.html','README.html'),
+include_files = [('README.rst','README.rst'), ('README.html','README.html'),
         ('README-ja.rst','README-ja.rst'), ('README-ja.html','README-ja.html')] \
         + create_include_doc() + create_include_tix()
 # modules?
@@ -73,10 +93,10 @@ setup(name = "suzu",
     version = vars['__version__'],
     description = "fake UI program for SRIM parameter input program (tin.exe)",
     author = "Takaaki AOKI",
-    author_email = "aoki@sakura.nucleng.kyoto-u.ac.jp",
-    url = "http://sakura.nucleng.kyoto-u.ac.jp/~aoki/suzu/",
+    author_email = "aoki.takaaki.6v@kyoto-u.ac.jp",
+    url = "https://github.com/takaakiaoki/suzu",
     download_url = "http://sakura.nucleng.kyoto-u.ac.jp/~aoki/hg/suzu/",
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     # cx_Freeze option
     options={'build_exe':build_exe_options},
     executables = [Executable('suzu.py', base='Console')]
